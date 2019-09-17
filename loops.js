5a3e7c4aedc01f87f0bf944a5238ca0aeab1ff90
Alex Meddock <alexmeddock@gmail.com>
2019-09-17 00:07:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function forLoop(array) {
+unction forLoop(array) {
    for ( let i = 0; i < 25; i++) {
      if (i === 1) {
        array.push(`I am 1 strange loop.`)
@@ -17,3 +17,13 @@ function whileLoop(n) {
     }
     return "done";
   }
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
